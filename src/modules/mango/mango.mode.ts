9cd34cccb44cba2792b5724ee235003bd842be5d
Ashiqur <ashiqur2812@gmail.com>
2025-06-24 00:15:12 +0600

    created mango interface and schema

---
+++ src/modules/mango/mango.mode.ts
@@ -0,0 +1,49 @@
+import { model, Schema } from "mongoose";
+import { IMango } from "./mango.interface";
+
+const mangoSchema = new Schema<IMango>({
+    name: {
+        type: String,
+        required: [true, 'mango name must be required'],
+        trim: true
+    },
+    variety: {
+        type: String,
+        required: [true, 'variety must be required'],
+        trim: true
+    },
+    unit: {
+        type: String,
+        enum: ['KG', 'TON'],
+        default: 'KG',
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: [0, '{VALUE} is not the valid price']
+    },
+    stock: {
+        type: Number,
+        required: true,
+        min: [0, '{VALUE} is not a valid stock number']
+    },
+    origin: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    season: {
+        type: String,
+        enum: ['Summer', 'Winter'],
+        default: 'Summer',
+    }
+},
+    {
+        versionKey: false,
+        timestamps: true
+    }
+);
+
+const Mango = model<IMango>('Mango', mangoSchema);
+export default Mango;
