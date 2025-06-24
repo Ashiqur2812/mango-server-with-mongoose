6189df61d9016c26a2ced965c909ff7301b0c3c4
Ashiqur <ashiqur2812@gmail.com>
2025-06-24 23:21:58 +0600

    embedded the address schema

--- src/modules/order/order.model.ts
+++ src/modules/order/order.model.ts
@@ -1,5 +1,12 @@
-import { Schema } from "mongoose";
+import { model, Schema } from "mongoose";
 import { IOrder } from "./order.interface";
+
+const orderAddress = new Schema({
+    zipCode: String,
+    state: String,
+    country: String,
+    street: String
+});
 
 const orderSchema = new Schema<IOrder>({
     user: {
@@ -22,12 +29,7 @@ const orderSchema = new Schema<IOrder>({
         required: true,
         min: [0, '{VALUE} is not a valid number']
     },
-    address: {
-        zipCode: String,
-        state: String,
-        country: String,
-        street: String
-    },
+    address: orderAddress,
     status: {
         type: String,
         required: true
@@ -37,3 +39,6 @@ const orderSchema = new Schema<IOrder>({
     timestamps: true
 }
 );
+
+const Order = model('Order', orderSchema);
+export default Order
